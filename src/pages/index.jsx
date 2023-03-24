9c5620015c5edb38c8cd7b123555ba4ba20ef995
nansamhayato0706 <nansama.hayato@gmail.com>
2023-03-24 16:11:46 +0900

    #9

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,18 @@
 import Head from "next/head";
+import { useCallback } from "react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
-import { Headline } from "../components/Headline";
-import { Links } from "../components/Links";
 import { Main } from "../components/Main";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    e.preventDefault();
+    console.log(e.target.href);
+    alert(foo);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +20,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <a
+        href="/about"
+        onClick={(e) => {
+          handleClick(e, foo);
+        }}
+      >
+        ボタン
+      </a>
       <Main page="index" />
 
       <Footer />
